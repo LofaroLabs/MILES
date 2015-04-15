48bf267da558ee6d30ba00edba7db0822473182f
Jenish Kastee <jkastee@gmu.edu>
2015-04-15 16:57:39 -0400

    Fixed the serial communication issues

--- Navigation/metareader/src/USB_window.cpp
+++ Navigation/metareader/src/USB_window.cpp
@@ -32,23 +32,37 @@ void USBWindow::on_buttonBox_accepted(){
 	asio::serial_port port(io);
 	port.open(ui.SerialPort->text().toStdString().c_str());
 	port.set_option(asio::serial_port_base::baud_rate(9600));
+	port.set_option(asio::serial_port::flow_control(asio::serial_port::flow_control::none));
+      	port.set_option(asio::serial_port::parity(asio::serial_port::parity::none));
+     	port.set_option(asio::serial_port::stop_bits(asio::serial_port::stop_bits::one));
+     	port.set_option(asio::serial_port::character_size(8));
 	std::string command="MD "+ui.buildNo->currentText().toStdString()+" "+ui.FloorNo->currentText().toStdString()+"\n";
-	asio::write(port,asio::buffer(command.c_str(),command.length()-1));
+	
+	asio::write(port,asio::buffer(command.c_str(),command.length()));
+	//asio::write(port,asio::buffer("\n",1));
 	printf("Reading\n");
 	std::string answer="";
 	while(1){
 		while(1){
 			char c;
-			printf("Before\n");
+			//printf("Before\n");
 			asio::read(port, asio::buffer(&c,1));
-			printf("After\n");
-			if(c=='\n')
+			//printf("After\n");
+			if(c=='\n'){
+				printf("Reached the end of line\n");
 				break;	
+			}
 			answer=answer+c;
-			printf("%s\n",answer.c_str());
+			
 		}
-		if(answer.find("<END>")!=std::string::npos)
+		printf("%s\n",answer.c_str());
+		
+		if(answer.find("<END>")!=std::string::npos){
+			printf("Reached the end of file\n");
 			break;		
+		}
+		
+		
 		QString lineString(answer.c_str());
 		QStringList info= lineString.split("||");
 		QStringList name=info[2].split(": ");
@@ -59,12 +73,13 @@ void USBWindow::on_buttonBox_accepted(){
 
 		QString Description=info[3];
 		print=Description.toStdString();
-
+		m_main_ui->Locations->addItem(Location,Description);
 		printf("%s\n",print.c_str());
+		answer="";
+		//printf("Finished Read\n");
 		
-		printf("Finished Read\n");
-	
 	}
+	printf("Finished Full Read\n");
 	port.close();
 }
 
